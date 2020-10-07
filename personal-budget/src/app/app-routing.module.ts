dce05c4fe9604f869b49f864244918b086fc78b2
antonybstack <ablyakhe@uncc.edu>
2020-10-07 15:49:27 -0400

    reorganize css, hero h1 tag home link, create breadcrumbs component, create contact page

--- personal-budget/src/app/app-routing.module.ts
+++ personal-budget/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
@@ -12,12 +13,16 @@ const routes: Routes = [
     pathMatch: 'full',
   },
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
     path: 'about',
     component: AboutComponent,
   },
   {
-    path: 'login',
-    component: LoginComponent,
+    path: 'contact',
+    component: ContactComponent,
   },
   {
     path: '**',
---
+++ personal-budget/src/app/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+@Component({
+  selector: 'pb-breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styleUrls: ['./breadcrumbs.component.scss'],
+})
+export class BreadcrumbsComponent implements OnInit {
+  // constructor(private router: Router) {}
+  // currentPage = this.router.url;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
